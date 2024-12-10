dbc1158d4163a2afb04258c2b91bd4fb0598759e
chrismitchell <chris.jnmitchell@gmail.com>
2024-12-10 12:36:52 -0700

    Netlify-ification

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,22 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
-export default function App() {
+import "./tailwind.css";
+
+export const links = () => [
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous",
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
+  },
+];
+
+export function Layout({ children }) {
   return (
     <html lang="en">
       <head>
@@ -16,10 +31,14 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
+}
+
+export default function App() {
+  return <Outlet />;
 }
